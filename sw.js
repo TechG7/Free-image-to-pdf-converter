487c6d418967b0385abd452962c43514241e5bcc
Gautam Kumar <gautamk8749@gmail.com>
2026-08-17 06:46:50 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('install', (e) => {
+ e.waitUntil(caches.open('pdf-tool').then((cache) => cache.addAll(['./index.html', './style.css', './script.js', './icon.png'])));
+});
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request))));
+
