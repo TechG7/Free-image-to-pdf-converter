2d2c87972031a5108914846a5b03f61c8a0cd649
Gautam Kumar <gautamk8749@gmail.com>
2026-09-12 20:04:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const ASSETS_TO_CACHE = [
   './index.html',
   './style.css',
   './script.js',
-  './icon.png',
+  './icons.png',
   './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.0/Sortable.min.js'
